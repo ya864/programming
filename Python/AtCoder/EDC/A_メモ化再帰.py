6407c22a2105b23b8e5acb14640a16e842534dd1
yuumakiakiya <ya86419@gmail.com>
2021-12-11 14:18:11 +0900

    アルゴリズム実技検定テキスト~p184

---
+++ Python/AtCoder/EDC/A_メモ化再帰.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+N -= 1
+
+h = list(map(int, input().split()))
+
+cost = [0 for i in range(N)]
+
+# １つ目のコストはスタート地点からの計算のみ
+cost[1] = cost[0] + abs(h[0] - h[1])
+
+# 足場が2個以上ある場合
+if N > 2:
+    for i in range(2, N):
+        h[i] = min(cost[i - 1] + abs(h[i-1] - h[i]), cost[i - 2] + abs(h[i-2] - h[i - 2]))
+
+print(h[N])
+
+aa
---
+++ Python/AtCoder/PAST/3/H.py
@@ -0,0 +1,30 @@
+N, L = map(int, input().split())
+hurdles = list(map(int, input().split()))
+# ハードルのある座標の登録
+H = [False for i in range(L+1)]
+for hurdle in hurdles:
+    H[hurdle] = True
+
+cost = [9999999999 for i in range(L+1)]
+
+T1, T2, T3 = map(int,input().split())
+
+for i in range(1, L+1):
+    # 行動1
+    cost[i] = min(cost[i], cost[i-1] + T1)
+    # 行動2
+    if i >= 2:
+        cost[i] = min(cost[i], cost[i-2] + T1 + T2)
+    # 行動3
+    if i >= 4:
+        cost[i] = min(cost[i], cost[i-4] + T1 + (3 * T2))
+    if H[i]:
+        cost[i] += T3
+    
+# 答えは座標Lに丁度止まるか、L-3〜L-1の座標からジャンプ中に到達したもの
+ans = cost[L]
+for i in [L-3, L-2, L-1]:
+    if i >= 0:
+        ans = min(ans, cost[i] + T1//2 + T2//2 + T2*(L-i))
+
+print(ans)
