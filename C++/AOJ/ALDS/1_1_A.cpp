7dac4e95d8f0e886274d846a29d729027b704abc
yuumakiakiya <ya86419@gmail.com>
2021-12-12 23:19:55 +0900

    C++アルゴリズムとデータ構造~p50

---
+++ C++/AOJ/ALDS/1_1_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+
+    int A[n];
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    
+    return 0;
+}
