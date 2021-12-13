867ea743b0c1536b8f6ccdfc33202f6f234ff9aa
yuumakiakiya <ya86419@gmail.com>
2021-12-13 16:15:23 +0900

    アルゴリズムとデータ構造~p59

--- C++/AOJ/ALDS/1_1_A.cpp
+++ C++/AOJ/ALDS/1_1_A.cpp
@@ -2,14 +2,40 @@
 #include <algorithm>
 using namespace std;
 
-int main(void){
-    int n;
-    cin >> n;
+void trace(int A[], int N) {
+    for (int i = 0; i < N; i++) {
+        if(i < N-1) {
+            cout << A[i] << " ";            
+        } else {
+            cout << A[i] << endl;
+        }
+    }
+}
 
-    int A[n];
-    for (int i = 0; i < n; i++) {
+void insertionSort(int A[], int N) {
+    int i, j, v;
+    for (i = 1; i < N; i++) {
+        v = A[i];
+        j = i - 1;
+        while (j >= 0 && A[j] > v) {
+            A[j+1] = A[j];
+            j--;
+        } 
+        A[j+1] = v;
+        trace(A, N);
+    }
+}
+
+int main(void){
+    int N;
+    cin >> N;
+
+    int A[N];
+    for (int i = 0; i < N; i++) {
         cin >> A[i];
     }
     
+    trace(A, N);
+    insertionSort(A, N);
     return 0;
 }
