9ae45d81f2daf01d9954a0d626e665a4014471da
akiya_yuumaki <ya86419@gmail.com>
2021-12-22 17:32:48 +0900

    アルゴリズム実技検定テキスト~p331

---
+++ Python/AtCoder/EducationalDP/H.py
@@ -0,0 +1,23 @@
+H, W = map(int, input().split())
+
+A = []
+for i in range(H):
+    A.append(input())
+
+cnt = [[0 for i in range(W)] for i in range(H)]
+cnt[0][0] = 1
+
+MOD = 10 ** 9 + 7
+
+for i in range(H):
+    for j in range(W):
+        if A[H][W] == '#':
+            continue
+        if i > 0:
+            cnt[i][j] = cnt[i - 1][j]
+        if j > 0:
+            cnt[i][j] = cnt[i][j - 1]
+
+    cnt[i][j] %= MOD
+
+print(cnt[i][j])
