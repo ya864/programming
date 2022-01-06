142615040662b1ac69d2302f92f95bf5c61bb941
akiya_yuumaki <ya86419@gmail.com>
2022-01-06 20:36:45 +0900

    線分の平行・直交_アルゴリズムとデータ構造~p375

---
+++ C++/AOJ/CGL/2_A_Parallel_Orthogonal.cpp
@@ -0,0 +1,120 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long llong;
+#define MAX 100000
+#define INF (1 << 30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+typedef pair<int, int> pi;
+
+// 点
+class Point
+{
+public:
+    double x, y;
+
+    Point(double x = 0, double y = 0) : x(x), y(y){};
+
+    Point operator+(Point p) { return Point(x + p.x, y + p.y); }
+    Point operator-(Point p) { return Point(x - p.x, y - p.y); }
+    Point operator*(double a) { return Point(x * a, y * a); }
+    Point operator/(double a) { return Point(x / a, y / a); }
+
+    double abs() { return sqrt(norm()); }
+    double norm() { return x * x + y * y; }
+
+    bool operator<(const Point &p) const
+    {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    bool operator==(const Point &p) const
+    {
+        return fabs(x - p.x) < EPS && fabs(y - p.y) < EPS;
+    }
+};
+
+// ベクトル
+typedef Point Vector;
+
+// ベクトルの内積の計算
+double dot(Vector a, Vector b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+// ベクトルの外積の計算
+double cross(Vector a, Vector b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+
+// 線分
+struct Segment
+{
+    Point p1, p2;
+    Segment(Point a, Point b)
+    {
+        p1 = a;
+        p2 = b;
+    }
+};
+
+// 線分が直行かを返す
+bool isOrthogonal(Segment s1, Segment s2) {
+    return equals(dot(s1.p2 - s1.p1, s2.p2 - s2.p1), 0.0);
+}
+
+// 線分が平行かを返す
+bool isParallel(Segment s1, Segment s2) {
+    return equals(cross(s1.p2 - s1.p1, s2.p2 - s2.p1), 0.0);
+}
+
+// 直線
+typedef Segment Line;
+
+// 円
+class Circle
+{
+public:
+    Point c;
+    double r;
+    Circle(Point c = Point(), double r = 0.0) : c(c), r(r) {}
+};
+
+// 多角形
+typedef vector<Point> Polygon;
+
+int main(void)
+{
+    int q;
+    cin >> q;
+    double x1, y1, x2, y2, x3, y3, x4, y4;
+
+    for (int i = 0; i < q; i++)
+    {
+        cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+
+        Segment s1(Point(x1, y1), Point(x2, y2));
+        Segment s2(Point(x3, y3), Point(x4, y4));
+
+        if (isParallel(s1, s2)) {
+            cout << "2" << endl;
+        } else if (isOrthogonal(s1, s2)) {
+            cout << "1" << endl;
+        } else {
+            cout << "0" << endl;
+        }
+    }
+    return 0;
+}
