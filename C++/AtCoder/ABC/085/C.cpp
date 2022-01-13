5737fda1d620022fae395e6f80eb38fcd27e37cb
akiya_yuumaki <ya86419@gmail.com>
2022-01-13 14:57:59 +0900

    ABC085_cpp

--- C++/AtCoder/ABC/085/C.cpp
+++ C++/AtCoder/ABC/085/C.cpp
@@ -19,6 +19,23 @@ typedef long long llong;
 
 int main(void)
 {
-    
+    int N, Y;
+    cin >> N >> Y;
+
+    int A = -1, B = -1, C = -1;
+    for (int a = 0; a <= N; a++){
+        for (int b = 0; a + b <= N; b++) {
+            int c = N - a - b;
+            int num = a * 10000 + b * 5000 + c * 1000;
+            if (num == Y && a + b + c == N) {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+    }
+
+    cout << A << " " << B << " " << C << endl;
+
     return 0;
 }
