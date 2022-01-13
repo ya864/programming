6c5431d4b4ad8bd762ca735a18f87f02765ba3e8
akiya_yuumaki <ya86419@gmail.com>
2022-01-13 16:27:43 +0900

    ABC086_C

--- C++/AtCoder/ABC/086/C.cpp
+++ C++/AtCoder/ABC/086/C.cpp
@@ -19,6 +19,20 @@ typedef long long llong;
 
 int main(void)
 {
+    int N;
+    cin >> N;
+    bool can = true;
     
+    int dist = 0, dt = 0, t = 0, x, y;
+    for (int i = 0; i < N; i++) {
+        cin >> t >> x >> y;
+        dt = t - dt;
+        dist = abs(x + y - dist);
+        if (dt < dist) can = false;
+        if (dist % 2 != dt % 2) can = false;
+    }
+
+    if (can) cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
