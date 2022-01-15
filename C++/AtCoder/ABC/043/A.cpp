5e9790bf55489ff6a2d76ea5a1e10ce05f777418
akiya_yuumaki <ya86419@gmail.com>
2022-01-15 20:48:53 +0900

    ABC043_A

---
+++ C++/AtCoder/ABC/043/A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long llong;
+#define MAX 100000
+#define INF (1 << 30)
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++){
+        ans += i;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
